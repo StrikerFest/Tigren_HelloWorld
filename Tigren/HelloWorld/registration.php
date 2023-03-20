a1659df9ebb52152044cf2c076ddd4979c46e560
The Anh <trinhtheanh789@Gmail.com>
2023-03-20 13:31:22 +0700

    Add copyright

--- Tigren/HelloWorld/registration.php
+++ Tigren/HelloWorld/registration.php
@@ -1,4 +1,9 @@
 <?php
+/**@author Trinhtheanh789 */
+/*
+ * Copyright (c) 2023.  Trinhtheanh789. All rights reserved.
+ */
+
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Tigren_HelloWorld',
